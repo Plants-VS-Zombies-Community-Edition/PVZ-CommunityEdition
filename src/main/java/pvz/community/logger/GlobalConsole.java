5f0ce2c5d5a1c1d961bafa650e5803019eeaacf8
Tofaa <82680183+Tofaa2@users.noreply.github.com>
2022-06-28 20:22:24 +0300

    Update GlobalConsole.java

--- src/main/java/pvz/community/logger/GlobalConsole.java
+++ src/main/java/pvz/community/logger/GlobalConsole.java
@@ -1,12 +1,13 @@
 package pvz.community.logger;
 
 import lombok.Getter;
+import pvz.community.launcher.GameLauncher;
 import pvz.community.logger.types.LinuxLogger;
 import pvz.community.logger.types.WindowsLogger;
 
 public class GlobalConsole {
 
-    @Getter private static final Logger logger = (System.getProperty("os.name").toLowerCase().contains("windows")) ? new WindowsLogger() : new LinuxLogger();
+    @Getter private static final Logger logger = (GameLauncher.OS.contains("windows")) ? new WindowsLogger() : new LinuxLogger();
 
 
 
